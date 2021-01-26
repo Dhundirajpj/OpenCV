e3ad0d19c7166420fde94ad13a18176fa3fa3063
Dhundiraj <dhundirajj00@gmail.com>
2021-01-26 21:06:55 +0530

    Second Commit

--- shapes.py
+++ shapes.py
@@ -9,6 +9,6 @@ img=np.zeros((600,600,3),np.uint8) #Black Image
 cv2.line(img,(0,0),(300,300),(0,255,0),3)
 cv2.rectangle(img,(0,0),(250,350),(0,0,255),cv2.FILLED) #Filled Rectangle
 cv2.circle(img,(400,50),30,(255,255,255),cv2.FILLED)
-cv2.putText(img,"Dhundiraj",(300,150),cv2.FONT_ITALIC,2,(0,150,0),3)
+cv2.putText(img,"Dhundi",(300,150),cv2.FONT_ITALIC,2,(0,150,0),3)
 cv2.imshow("Image",img)
 cv2.waitKey(0)
